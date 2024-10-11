b4217fe5fbaffe5c34ae5e73d72843bd61bb31fc
PhilipeF <philipefdev@gmail.com>
2024-10-11 11:07:01 -0300

    Resolucao do exercicio ok

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        const commetTextarea = screen.getByTestId('comment-textarea');
+        const submitButton = screen.getByTestId('comment-submit');
+        const comentsList = screen.getByTestId('comments-list');
+
+        fireEvent.change(commetTextarea, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(submitButton);
+
+        fireEvent.change(commetTextarea, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(submitButton);
+
+        const commentItems = screen.getAllByTestId('comment-item');
+        expect(commentItems.length).toBe(2);
+        expect(comentsList).toHaveTextContent('Primeiro comentário');
+        expect(comentsList).toHaveTextContent('Segundo comentário');
+    })
 });
